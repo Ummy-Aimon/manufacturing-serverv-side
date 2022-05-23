0d22f617b4436918779fa7863c0f454205c47332
Ummy-Aimon <ummyatia@gmail.com>
2022-05-23 23:04:39 +0600

    mongodb Connect

--- index.js
+++ index.js
@@ -3,25 +3,55 @@ const cors =require ('cors');
 require('dotenv').config()
 const port= process.env.PORT || 5000
 const app= express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 
 // Middelware
 app.use(cors())
 app.use(express.json())
 
+// MongoDB Atlast
 
-const uri = `mongodb+srv://${process.env.Paint_User}:${process.env.Paint_Pass}@cluster0.0ergu.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ergu.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
+async function run (){
+    try{
+        await client.connect();
+        const PaintBlushCollection= client.db('PaintBlush').collection('Tools')
+       
+        // PaintBlush get
+        app.get('/tools', async (req,res) => {
+            const query={}
+            const cursor= PaintBlushCollection.find(query)
+            const PaintBlushItem= await cursor.toArray()
+            console.log(PaintBlushItem)
+            res.send(PaintBlushItem)
+        
+            })
+            
+        //    PaintBlush Param
 
+            app.get('/tools/:id', async(req,res)=>{
+                const id= req.params.id
+                const query={_id:ObjectId(id)}
+                const PaintItem =await PaintBlushCollection.findOne(query)
+                res.send(PaintItem)
+    
+            })
 
+        // PaintBlush POST
+        app.post('/tools', async (req,res)=>{
+            const newtool=req.body
+            const toolresult= await PaintBlushCollection.insertOne(newtool)
+            res.send(toolresult)
+        })
+    }
+    finally{
 
-
+    }
+}
+run().catch(console.dir)
 
 app.get('/',(req,res)=>{
     res.send('Running painblush manufacturing tools')
