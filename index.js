36605e64499bc1d6ee5ff866c79ccc99c0f5697c
Ummy-Aimon <ummyatia@gmail.com>
2022-05-26 00:27:15 +0600

    admin put

--- index.js
+++ index.js
@@ -20,6 +20,28 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// verify token 
+
+function verifyJWT(req,res,next){
+const authHeader = req.headers.authorization
+
+if(!authHeader){
+    return res.status(401).send({message: "unAuthorized Access"})
+}
+
+const token= authHeader.split('')[1]
+
+  jwt.verify(token,process.env.JWT_TOKEN, function(err, decoded){
+      if(err){
+          return res.status(403).send({message:"Forbidded Access"})
+      }
+      req.decoded= decoded
+      next()
+})
+
+
+}
+
 async function run (){
     try{
         await client.connect();
@@ -75,25 +97,41 @@ async function run (){
 
         // PainBlush user get
     
-        app.get('/user',async(req, res)=>{
+        app.get('/user',verifyJWT,async(req, res)=>{
             const users= await UserCollection.find().toArray()
             res.send(users)
 
         })
 
+        // PainBlush admin get
+
+        app.get('/admin/:email', async (req,res)=>{
+            const email= req.params.email
+            const user= await UserCollection.findOne({email:email})
+            const isAdmin = user.role === 'admin'
+            res.send(isAdmin)
+        })
+
         // PaintBlush Admin Put
-        app.put('/user/admin/:email', async (req,res)=>{
+        app.put('/user/admin/:email' , verifyJWT,async (req,res)=>{
             const email=req.params.email
-            console.log(email)
-            const filter= { email:email}
-            const updateDoc={
-                $set: {role:"admin"}
-                
-            };
-          const result= await UserCollection.updateOne
+            const requst= req.decoded.email
+            const requestAccount= await UserCollection.findOne({ email: requst})
+            if(requestAccount.role === 'admin'){
+                const filter= { email:email}
+                const updateDoc={
+                    $set: {role:"admin"}
+                    
+            }
+            const result= await UserCollection.updateOne
           (filter, updateDoc )
-          const token=jwt.sign({email:email}, process.env.JWT_TOKEN,{ expiresIn: '1h' })
-            res.send({result,token})
+          res.send(result)  
+         }
+         else{
+             res.status(403).send({message:"Forbidded Access"}) 
+         }
+        //   const token=jwt.sign({email:email}, process.env.JWT_TOKEN,{ expiresIn: '1h' })
+        
           })
          
         // PaintBlush user Put
